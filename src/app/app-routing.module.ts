8e284e43ab78255155b7423116882137a2e55c90
anddzi <andrei.dzilanian@gmail.com>
2024-02-13 18:22:48 +0100

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './pages/main/main.component';
+import { LoginRegisterComponent } from './pages/auth/login-register/login-register.component';
+import { P404Component } from './pages/p404/p404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/main', pathMatch: 'full' },
+  { path: 'main', component: MainComponent },
+  { path: 'auth', component: LoginRegisterComponent },
+  { path: 'p404', component: P404Component },
+  { path: '**', redirectTo: '/p404' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { P404Component } from './pages/p404/p404.component';
+import { LoginRegisterComponent } from './pages/auth/login-register/login-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    P404Component,
+    LoginRegisterComponent
   ],
   imports: [
     BrowserModule,
