530b62b4c847fcc0b45f2c2c54937c678de5c00e
anddzi <andrei.dzilanian@gmail.com>
2024-02-20 19:30:18 +0100

    added socket

--- src/app/modules/game-runner/game-runner.module.ts
+++ src/app/modules/game-runner/game-runner.module.ts
@@ -1,19 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+//
 import { GameRunnerRoutingModule } from './game-runner-routing.module';
 import { StartComponent } from './pages/start/start.component';
 import { GameComponent } from './pages/game/game.component';
 
+const config: SocketIoConfig = { url: 'http://localhost:8988', options: {} };
 
 @NgModule({
-  declarations: [
-    StartComponent,
-    GameComponent
-  ],
+  declarations: [StartComponent, GameComponent],
   imports: [
     CommonModule,
-    GameRunnerRoutingModule
-  ]
+    GameRunnerRoutingModule,
+    SocketIoModule.forRoot(config),
+  ],
 })
-export class GameRunnerModule { }
+export class GameRunnerModule {}
